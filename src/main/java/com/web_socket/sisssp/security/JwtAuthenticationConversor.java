e8c771d76d94dd87769eb9aa889abb7d71d843e1
Valberth.lisboa <valbertth.lisboa@ssp.ma.gov.br>
2026-05-07 16:51:57 -0300

    feat: add security

--- src/main/java/com/web_socket/sisssp/security/JwtAuthenticationConversor.java
+++ src/main/java/com/web_socket/sisssp/security/JwtAuthenticationConversor.java
@@ -1,4 +1,24 @@
 package com.web_socket.sisssp.security;
 
-public class JwtAuthenticationConversor {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtAuthenticationConversor implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        List<String> perfis = jwt.getClaimAsStringList("perfis");
+        if (perfis == null) {
+            perfis = Collections.emptyList();
+        }
+        return perfis.stream()
+                .map(p -> new SimpleGrantedAuthority("ROLE_" + p.toUpperCase()))
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/web_socket/sisssp/security/SecurityConfiguration.java
+++ src/main/java/com/web_socket/sisssp/security/SecurityConfiguration.java
@@ -1,12 +1,31 @@
 package com.web_socket.sisssp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain filtroManager(HttpSecurity http) throws Exception {
+        return http
+                .anonymous(config-> config.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(
+                        auth -> auth
+                                .anyRequest().permitAll()
+                ).build();
+    }
 
-
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
+    }
 }
