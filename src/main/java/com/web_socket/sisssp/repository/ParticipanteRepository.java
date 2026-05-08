504ef2288e6ccd370a69601d2c4e3a51be0e26e0
Valberth.lisboa <valbertth.lisboa@ssp.ma.gov.br>
2026-05-08 16:06:58 -0300

    feat: criação do metodo repository "buscarConversasPorUsuario".'

--- src/main/java/com/web_socket/sisssp/repository/ParticipanteRepository.java
+++ src/main/java/com/web_socket/sisssp/repository/ParticipanteRepository.java
@@ -1,8 +1,12 @@
 package com.web_socket.sisssp.repository;
 
 import com.web_socket.sisssp.domain.Participante;
+import com.web_socket.sisssp.projection.ConversaUsuarioProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -19,5 +23,22 @@ public interface ParticipanteRepository extends JpaRepository<Participante, UUID
          AND c.tipo_conversa = 'PRIVADA'
        LIMIT 1
     """, nativeQuery = true)
-    Optional<UUID> findExistingConversationId(String userA, String userB);
+    Optional<UUID> encontrarIdDeConversaExistente(String userA, String userB);
+
+
+    @Query(value = """
+        SELECT 
+            c.id AS conversa_id,
+            c.data_conversa,
+            c.tipo_conversa,
+            p2.usuario_externo_id AS nome_destinatario
+        FROM public.tb_conversas c
+        JOIN public.tb_participantes p1 ON c.id = p1.conversa_id
+        JOIN public.tb_participantes p2 ON c.id = p2.conversa_id
+        WHERE p1.usuario_externo_id = :usuarioId
+          AND p2.usuario_externo_id <> :usuarioId
+        ORDER BY c.data_conversa DESC
+        """, nativeQuery = true)
+    Page<ConversaUsuarioProjection> buscarConversasPorUsuario(@Param("usuarioId") String usuarioId, Pageable pageable);
+
 }
