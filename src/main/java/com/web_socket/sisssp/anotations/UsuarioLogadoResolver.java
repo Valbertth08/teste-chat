5c2b0aedad9c7df584063c5e63f3127ca97a22ea
Valbertth08 <valbertthestudos@gmail.com>
2026-05-11 23:49:10 -0300

    feat: adição das conifgs para buscar o subject do usuario logado.

--- src/main/java/com/web_socket/sisssp/anotations/UsuarioLogadoResolver.java
+++ src/main/java/com/web_socket/sisssp/anotations/UsuarioLogadoResolver.java
@@ -2,6 +2,8 @@ package com.web_socket.sisssp.anotations;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -16,11 +18,22 @@ public class UsuarioLogadoResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(UsuarioLogado.class) &&
-                parameter.getParameterType().equals(UUID.class);
+                parameter.getParameterType().equals(String.class);
     }
 
     @Override
     public @Nullable Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer, NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {
-        return null;
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new IllegalStateException("Usuário não autenticado.");
+        }
+
+        Object principal = auth.getPrincipal();
+
+        if (!(principal instanceof Jwt jwt)) {
+            throw new IllegalStateException("Autenticação JWT inválida.");
+        }
+        return jwt.getSubject();
     }
 }
