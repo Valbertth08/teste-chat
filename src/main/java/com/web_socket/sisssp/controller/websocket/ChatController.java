8b31359cb75e4f8d4cdd2ba68d9d2b9c51c5bccb
Valberth.lisboa <valbertth.lisboa@ssp.ma.gov.br>
2026-05-15 16:23:19 -0300

    feat: adicionando configuração do RabbitMQ para criação de filas de usuários.

--- src/main/java/com/web_socket/sisssp/controller/websocket/ChatController.java
+++ src/main/java/com/web_socket/sisssp/controller/websocket/ChatController.java
@@ -1,8 +1,10 @@
 package com.web_socket.sisssp.controller.websocket;
 
+import com.web_socket.sisssp.config.RabbitMqConfig;
 import com.web_socket.sisssp.dto.MensagemPrivadaEntradaDTO;
 import com.web_socket.sisssp.dto.MensagemPrivadaRespostaDTO;
 import com.web_socket.sisssp.service.ChatService;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,21 +14,25 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
-
     private final ChatService chatService;
-    public ChatController(SimpMessagingTemplate messagingTemplate,  ChatService chatService) {
+    private final RabbitAdmin rabbitAdmin;
+    private final RabbitMqConfig rabbitMqConfig;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate, ChatService chatService, RabbitAdmin rabbitAdmin, RabbitMqConfig rabbitMqConfig) {
         this.messagingTemplate = messagingTemplate;
         this.chatService = chatService;
+        this.rabbitAdmin = rabbitAdmin;
+        this.rabbitMqConfig = rabbitMqConfig;
     }
 
     @MessageMapping("/enviar-mensagem-privada.send")
     public void enviarMensagemPrivada(@Payload MensagemPrivadaEntradaDTO dados) {
-        MensagemPrivadaRespostaDTO respostaDTO = chatService.salvarMensagemPrivada(dados, dados.usuarioRemetente());
+        rabbitMqConfig.criarFilaUsuario(dados.usuarioDestinatario(), rabbitAdmin);
+        MensagemPrivadaRespostaDTO respostaDTO = chatService
+                .salvarMensagemPrivada(dados, dados.usuarioRemetente());
         messagingTemplate.convertAndSend(
                 "/queue/user." + dados.usuarioDestinatario(),
                 respostaDTO
         );
-
     }
-
 }
